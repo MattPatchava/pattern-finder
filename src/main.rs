515ba2410558242cbea870535d501da9b8a2a104
Matt Patchava <mattpatchava@outlook.com>
2025-08-10 18:26:24 +1000

    feat: add hex pattern validation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use anyhow::{Context, Result, anyhow};
 use clap::{Parser, ValueEnum};
 
 #[derive(Parser)]
@@ -31,11 +32,35 @@ impl std::fmt::Display for HashingProtocol {
     }
 }
 
+fn validate_hex_pattern(s: &str, max_length: usize) -> Result<String, String> {
+    if s.is_empty() {
+        return Err(String::from("Pattern cannot be empty."));
+    }
+
+    if s.len() > max_length {
+        return Err("Pattern cannot exceed the length of the hash digest.".to_string());
+    }
+
+    if !s.chars().all(|c| c.is_ascii_hexdigit()) {
+        return Err("Pattern must only contain hexadecimal digits.".into());
+    }
+
+    Ok(s.to_lowercase())
+}
+
 fn main() {
     let args: Args = Args::parse();
 
+    let pattern: String = match validate_hex_pattern(&args.pattern, 64) {
+        Ok(p) => p,
+        Err(e) => {
+            eprintln!("{}", e);
+            std::process::exit(1);
+        }
+    };
+
     println!(
         "Pattern to match: {}\nHashing protocol: {}\nMax input length: {}",
-        args.pattern, args.protocol, args.input_length
+        pattern, args.protocol, args.input_length
     );
 }
