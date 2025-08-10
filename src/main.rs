60f5b33d55c71240a3fb5c64775e06c2f88e2620
Matt Patchava <mattpatchava@outlook.com>
2025-08-10 10:44:50 +1000

    feat: add argv parsing
    Added parsing for CLI arguments, including:
    - Pattern to match
    - Hashing protocol
    - Maximum input length

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,41 @@
+use clap::{Parser, ValueEnum};
+
+#[derive(Parser)]
+struct Args {
+    #[arg(long)]
+    pattern: String,
+
+    #[arg(long, default_value_t = HashingProtocol::Sha256)]
+    protocol: HashingProtocol,
+
+    #[arg(long, default_value_t = 6)]
+    input_length: usize,
+}
+
+#[derive(ValueEnum, Clone)]
+enum HashingProtocol {
+    Sha256,
+    Md5,
+}
+
+impl std::fmt::Display for HashingProtocol {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> Result<(), std::fmt::Error> {
+        write!(
+            f,
+            "{}",
+            match self {
+                HashingProtocol::Sha256 => "sha256",
+                HashingProtocol::Md5 => "md5",
+            }
+        )
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args: Args = Args::parse();
+
+    println!(
+        "Pattern to match: {}\nHashing protocol: {}\nMax input length: {}",
+        args.pattern, args.protocol, args.input_length
+    );
 }
